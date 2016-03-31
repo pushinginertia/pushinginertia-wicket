ff2bcb1954aa0a1ddb9b22d65a1d04f6a3be1ad4
Dan <dan.traczynski@gmail.com>
2016-03-31 14:22:30 +0100

    added StatelessBooleanRadio

---
+++ pushinginertia-wicket-core/src/main/java/com/pushinginertia/wicket/core/form/component/StatelessBooleanRadio.java
@@ -0,0 +1,43 @@
+/* Copyright (c) 2011-2016 Pushing Inertia
+ * All rights reserved.  http://pushinginertia.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.pushinginertia.wicket.core.form.component;
+
+import org.apache.wicket.markup.html.form.Radio;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Provides a stateless radio button for boolean values. The difference is that the 'value' attribute in the html
+ * input tag will be 'true' or 'false' instead of a wicket-generated incrementing value specific to the session.
+ */
+public class StatelessBooleanRadio extends Radio<Boolean> {
+	private final boolean value;
+
+	public StatelessBooleanRadio(@Nonnull final String id, final boolean value) {
+		super(id);
+		this.value = value;
+	}
+
+	@Override
+	public String getValue() {
+		return Boolean.toString(value);
+	}
+
+	@Override
+	protected boolean getStatelessHint() {
+		return true;
+	}
+}
