608c76bb9122ff5550880a24979ac975263f036a
Dan <dan.traczynski@gmail.com>
2016-05-22 16:17:04 +0100

    Added a unicode test for NumberSequenceContentReplacer

--- pushinginertia-wicket-core/src/main/java/com/pushinginertia/wicket/core/model/replacement/NumberSequenceContentReplacer.java
+++ pushinginertia-wicket-core/src/main/java/com/pushinginertia/wicket/core/model/replacement/NumberSequenceContentReplacer.java
@@ -17,10 +17,11 @@ public abstract class NumberSequenceContentReplacer implements ContentReplacer {
 	 */
 	public static final String NUM =
 			"([0-9\u2460-\u249b\u24ea-\u24ff\u2776-\u2793\u0030-\u0039\uff10-\uff19]|zero|one|two|three|four|five|six|seven|eight|nine)";
+	public static final String SEP = "([\\(\\)\\., \u3000-]*";
 	/**
 	 * Matches a sequence of numbers that might also be written as words with various punctuation in between.
 	 */
-	private static final String BASE_REGEX_PRE = "(\\(\\s*)?" + NUM + "([\\(\\)\\., \u3000-]*" + NUM + "){";
+	private static final String BASE_REGEX_PRE = ".*(\\(\\s*)?" + NUM + SEP + NUM + "){";
 	private static final String BASE_REGEX_POST = ",}";
 
 	private final String pattern;
--- pushinginertia-wicket-core/src/test/java/com/pushinginertia/wicket/core/model/replacement/NumberSequenceContentReplacerTest.java
+++ pushinginertia-wicket-core/src/test/java/com/pushinginertia/wicket/core/model/replacement/NumberSequenceContentReplacerTest.java
@@ -16,5 +16,8 @@ public class NumberSequenceContentReplacerTest {
 			}
 		};
 		Assert.assertTrue("( 123 ) 456.7890".matches(replacer.pattern()));
+		// test some unicode numbers and spaces
+		Assert.assertTrue(
+				"\uff11\uff12\uff13\u3000\uff14\uff15\uff16\u3000\uff19\uff19\uff19\uff19".matches(replacer.pattern()));
 	}
 }
