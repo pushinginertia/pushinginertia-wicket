ffdf3fbb94d33a34fa620cd55af87eb3ac08a8ae
Dan <dan.traczynski@gmail.com>
2017-07-27 20:59:00 +0100

    New model StripLeadingTrailingCharactersModel

---
+++ pushinginertia-wicket-core/src/main/java/com/pushinginertia/wicket/core/form/model/StripLeadingTrailingCharactersModel.java
@@ -0,0 +1,62 @@
+/* Copyright (c) 2011-2017 Pushing Inertia
+ * All rights reserved.  http://pushinginertia.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.pushinginertia.wicket.core.form.model;
+
+import com.pushinginertia.commons.lang.StringUtils;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nonnull;
+
+/**
+ * A model that can be attached to a form input, which will strip a given list characters from the
+ * start and end of the input entered by the user. This wraps another model that represents the
+ * string that the form input will be assigned to.
+ */
+public class StripLeadingTrailingCharactersModel extends Model<String> {
+	private static final long serialVersionUID = 1L;
+	private static final Logger LOG = LoggerFactory.getLogger(StripLeadingTrailingCharactersModel.class);
+
+	private final IModel<String> nestedModel;
+	private final char[] illegalChars;
+
+	public StripLeadingTrailingCharactersModel(
+			@Nonnull final IModel<String> model,
+			@Nonnull final char[] illegalChars) {
+		this.nestedModel = model;
+		this.illegalChars = illegalChars;
+	}
+
+	@Override
+	public String getObject() {
+		return nestedModel.getObject();
+	}
+
+	@Override
+	public void setObject(final String value) {
+		if (value == null) {
+			nestedModel.setObject(null);
+		} else {
+			final String stripped = StringUtils.stripLeadingTrailingCharacters(value, illegalChars).trim();
+			if (stripped.length() != value.length()) {
+				LOG.info("Stripped characters from string [" + value + "] to produce [" + stripped + "].");
+			}
+			nestedModel.setObject(stripped);
+		}
+	}
+}
---
+++ pushinginertia-wicket-core/src/test/java/com/pushinginertia/wicket/core/form/model/StripLeadingTrailingCharactersModelTest.java
@@ -0,0 +1,39 @@
+/* Copyright (c) 2011-2017 Pushing Inertia
+ * All rights reserved.  http://pushinginertia.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.pushinginertia.wicket.core.form.model;
+
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.util.tester.WicketTester;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StripLeadingTrailingCharactersModelTest {
+	private final WicketTester tester = new WicketTester();
+
+	private static class TestBean {
+		private String name;
+	}
+
+	@Test
+	public void setObject() {
+		final TestBean bean = new TestBean();
+		bean.name = "abcd";
+		final PropertyModel<String> innerModel = new PropertyModel<>(bean, "name");
+		final RemoveCharactersModel model = new RemoveCharactersModel(innerModel, "~!@#$%^&*()".toCharArray());
+		model.setObject("~!abc*");
+		Assert.assertEquals("abc", model.getObject());
+	}
+}
