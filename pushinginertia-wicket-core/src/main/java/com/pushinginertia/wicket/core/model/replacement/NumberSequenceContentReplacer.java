80de0664a22320e14b45f2119e3f5792785ee5e5
Dan <dan.traczynski@gmail.com>
2017-08-13 15:20:19 +0100

    Increased coverage for other ways numbers might be obfuscated

--- pushinginertia-wicket-core/src/main/java/com/pushinginertia/wicket/core/model/replacement/NumberSequenceContentReplacer.java
+++ pushinginertia-wicket-core/src/main/java/com/pushinginertia/wicket/core/model/replacement/NumberSequenceContentReplacer.java
@@ -12,7 +12,7 @@ public abstract class NumberSequenceContentReplacer implements ContentReplacer {
 	/**
 	 * Represents the numbers 0-10 in Chinese. Zero has two representations and both are reflected here.
  	 */
-	public static final String NUM_ZH =
+	private static final String NUM_ZH =
 			"\u96f6\u3007\u4e00\u4e8c\u4e09\u56db\u4e94\u516d\u4e03\u516b\u4e5d\u5341";
 	/**
 	 * Matches various ways one might write a number.
@@ -20,16 +20,19 @@ public abstract class NumberSequenceContentReplacer implements ContentReplacer {
 	 * http://www.fileformat.info/info/unicode/category/No/list.htm
 	 * http://www.fileformat.info/info/unicode/category/Nd/list.htm
 	 */
-	public static final String NUM =
+	private static final String NUM =
 			"([0-9\u2460-\u249b\u24ea-\u24ff\u2776-\u2793\u0030-\u0039\uff10-\uff19" +
 			NUM_ZH +
-			"]|[cz]ero|one|two|three|four|five|six|seven|eight|nine)";
-	public static final String SEP = "([\\(\\)\\., \u3000-]*";
+			"]|[cz]ero|oh|one|two|three|four|five|six|seven|eight|nine)";
+	/**
+	 * u+ff08 and u+ff09 are fixed width opening and closing parentheses.
+	 */
+	private static final String SEP = "([\\(\\)\\._*, \u3000\uff08\uff09–-]*";
 	/**
 	 * Matches a sequence of numbers that might also be written as words with various punctuation in between.
 	 */
-	private static final String BASE_REGEX_PRE = "(\\(\\s*)?" + NUM + SEP + NUM + "){";
-	private static final String BASE_REGEX_POST = ",}";
+	private static final String BASE_REGEX_PRE = "([\\(\uff08]\\s*)?" + NUM + SEP + NUM + "){";
+	private static final String BASE_REGEX_POST = ",}(\\s*[\uff09\\)])?";
 
 	private final String pattern;
 
--- pushinginertia-wicket-core/src/test/java/com/pushinginertia/wicket/core/model/replacement/NumberSequenceContentReplacerTest.java
+++ pushinginertia-wicket-core/src/test/java/com/pushinginertia/wicket/core/model/replacement/NumberSequenceContentReplacerTest.java
@@ -4,27 +4,48 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import javax.annotation.Nonnull;
+import java.text.MessageFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class NumberSequenceContentReplacerTest {
+	private final NumberSequenceContentReplacer replacer = new NumberSequenceContentReplacer(8) {
+		@Nonnull
+		@Override
+		public String replacement() {
+			return "";
+		}
+	};
+	private Pattern replacerPattern =
+			Pattern.compile(replacer.pattern(), Pattern.CASE_INSENSITIVE);
+
+	private void assertStringMatchesNumberSequencePattern(final String s) {
+		// ContentReplacerList performs a case insensitive pattern match
+		final Matcher m = replacerPattern.matcher(s);
+		Assert.assertTrue(
+				MessageFormat.format(
+						"String [{0}] does not match pattern: {1}",
+						s,
+						replacer.pattern()),
+				m.matches());
+	}
+
 	@Test
 	public void regex() {
-		final NumberSequenceContentReplacer replacer = new NumberSequenceContentReplacer(8) {
-			@Nonnull
-			@Override
-			public String replacement() {
-				return "";
-			}
-		};
-		Assert.assertTrue("( 123 ) 456.7890".matches(replacer.pattern()));
-
-		// words and numbers combined
-		Assert.assertTrue("9zero9one2three4five6seven".matches(replacer.pattern()));
-		Assert.assertTrue("9zero9 one2three4five6seven".matches(replacer.pattern()));
-		Assert.assertTrue("0. 1. 2. 3 45 6 7. 8 9".matches(replacer.pattern()));
+		assertStringMatchesNumberSequencePattern("( 123 ) 456.7890");
+		assertStringMatchesNumberSequencePattern("1-2-3-4-5-6-7-8-9-0");
+		assertStringMatchesNumberSequencePattern("123 – 456- 7890");
+		assertStringMatchesNumberSequencePattern("9zero9one2three4five6seven");
+		assertStringMatchesNumberSequencePattern("9zero9 one2three4five6seven");
+		assertStringMatchesNumberSequencePattern("0. 1. 2. 3 45 6 7. 8 9");
+		assertStringMatchesNumberSequencePattern("（123）（456）（7890）");
+		assertStringMatchesNumberSequencePattern("one two three, zero nine two, four three two oh");
+		assertStringMatchesNumberSequencePattern("( 123 ) 456-7890");
+		assertStringMatchesNumberSequencePattern("one two _three____four___five___six__ seven__ eight__ nine__ zero");
 
 		// test some unicode numbers and spaces
-		Assert.assertTrue("\u4e03\u516b\u4e5d\u5341---\u4e09\u56db\u4e94\u516d".matches(replacer.pattern()));
-		Assert.assertTrue(
-				"\uff11\uff12\uff13\u3000\uff14\uff15\uff16\u3000\uff19\uff19\uff19\uff19".matches(replacer.pattern()));
+		assertStringMatchesNumberSequencePattern("\u4e03\u516b\u4e5d\u5341---\u4e09\u56db\u4e94\u516d");
+		assertStringMatchesNumberSequencePattern(
+				"\uff11\uff12\uff13\u3000\uff14\uff15\uff16\u3000\uff19\uff19\uff19\uff19");
 	}
 }
