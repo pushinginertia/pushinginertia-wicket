46cc373108874ee5131f6b3b4ffac8811f412b01
Dan <dan.traczynski@gmail.com>
2018-01-21 19:03:09 +0000

    Test another number sequence variation

--- pushinginertia-wicket-core/src/test/java/com/pushinginertia/wicket/core/model/replacement/NumberSequenceContentReplacerTest.java
+++ pushinginertia-wicket-core/src/test/java/com/pushinginertia/wicket/core/model/replacement/NumberSequenceContentReplacerTest.java
@@ -1,3 +1,18 @@
+/* Copyright (c) 2011-2018 Pushing Inertia
+ * All rights reserved.  http://pushinginertia.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.pushinginertia.wicket.core.model.replacement;
 
 import org.junit.Assert;
@@ -42,6 +57,7 @@ public class NumberSequenceContentReplacerTest {
 		assertStringMatchesNumberSequencePattern("one two three, zero nine two, four three two oh");
 		assertStringMatchesNumberSequencePattern("( 123 ) 456-7890");
 		assertStringMatchesNumberSequencePattern("one two _three____four___five___six__ seven__ eight__ nine__ zero");
+		assertStringMatchesNumberSequencePattern("oh one two - three four five- six seven eight nine");
 
 		// test some unicode numbers and spaces
 		assertStringMatchesNumberSequencePattern("\u4e03\u516b\u4e5d\u5341---\u4e09\u56db\u4e94\u516d");
